2d31fd7bf00a922a06b1a3f91c9f7fb3fc72b60e
SH4RD3D <jaramode68duck.com>
2025-11-21 10:41:41 -0500

    Quit

--- src/main.js
+++ src/main.js
@@ -57,3 +57,7 @@ app.whenReady().then(() => {
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit();
 });
+
+app.on('will-quit', () => {
+    globalShortcut.unregisterAll();
+});
