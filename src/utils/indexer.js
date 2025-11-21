8d21081c88c4813d59013cc09b8ecd6e55972177
SH4RD3D <jaramode68duck.com>
2025-11-21 12:38:20 -0500

    File indexer constructor

---
+++ src/utils/indexer.js
@@ -0,0 +1,16 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+class FileIndexer {
+    constructor() {
+        this.index = new Map();
+        this.isIndexing = false;
+        this.indexVersion = 1;
+        this.supportedFileTypes = new Set([
+            'txt', 'md', 'js', 'html', 'css', 'json', 'xml', 'csv',
+            'pdf', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 
+            'jpg', 'jpeg', 'png', 'gif', 'svg', 'mp3', 'mp4', 'avi',
+            'mov', 'wav', 'zip', 'rar', '7z'
+        ]);
+    }
+}
