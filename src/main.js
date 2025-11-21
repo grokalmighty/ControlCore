53406ddc44f2c6e19a955cc2008a8d567053b375
SH4RD3D <jaramode68duck.com>
2025-11-21 10:35:43 -0500

    Create window function

---
+++ src/main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow, globalShortcut, ipcMain, dialog } = require('electron');
+const path = require('path');
+const isDev = proces.argv.includes('--dev');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 600,
+        height: 500,
+        show: false,
+        frame: false,
+        alwaysOnTop: true,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+}
